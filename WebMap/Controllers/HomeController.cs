179e81ee95c1bd56331731468577ab5ea24001dd
rupalimetkari <rupalimetkari1@gmail.com>
2023-09-20 17:13:17 -0700

    test code csv working

--- WebMap/Controllers/HomeController.cs
+++ WebMap/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,7 +16,27 @@ namespace WebMap.Controllers
         {
             ViewBag.Title = "Home Page";
 
+
+            List<Location> locations = new List<Location>();
+
+            using (var reader = new StreamReader(Server.MapPath("~/Data/modifieddata.csv")))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                locations = csv.GetRecords<Location>().ToList();
+                var x = 5;
+            }
+
             return View();
         }
+    }
+
+
+    public class Location
+    {
+        [Name("LATITUDE")]
+        public double? Latitude { get; set; }
+
+        [Name("LONGITUDE")]
+        public double? Longitude { get; set; }
     }
 }
