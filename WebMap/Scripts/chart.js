8ab9b9cc914788d2ac51b6ee69646f83e9ce5b3c
rupalimetkari <rupalimetkari1@gmail.com>
2023-09-20 21:44:21 -0700

    chart working

--- WebMap/Scripts/chart.js
+++ WebMap/Scripts/chart.js
@@ -1,69 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-Highcharts.chart('highchartcontainer', {
-    chart: {
-        type: 'spline'
-    },
-    title: {
-        text: 'Monthly Average Temperature'
-    },
-    xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        accessibility: {
-            description: 'Months of the year'
-        }
-    },
-    yAxis: {
+    Highcharts.chart('highchartcontainer', {
+        chart: {
+            type: 'spline'
+        },
         title: {
-            text: 'Temperature'
+            text: 'Price Comparison'
         },
-        labels: {
-            format: '{value}°'
-        }
-    },
-    tooltip: {
-        crosshairs: true,
-        shared: true
-    },
-    plotOptions: {
-        spline: {
-            marker: {
-                radius: 4,
-                lineColor: '#666666',
-                lineWidth: 1
-            }
-        }
-    },
-    series: [{
-        name: 'Tokyo',
-        marker: {
-            symbol: 'square'
-        },
-        data: [5.2, 5.7, 8.7, 13.9, 18.2, 21.4, 25.0, {
-            y: 26.4,
-            marker: {
-                symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
+        xAxis: {
+            categories: siteNames,
+            labels: {
+                rotation: -45
             },
-            accessibility: {
-                description: 'Sunny symbol, this is the warmest point in the chart.'
+            min: 0, 
+            max: 10, 
+            scrollbar: {
+                enabled: true
             }
-        }, 22.8, 17.5, 12.1, 7.6]
-
-    }, {
-        name: 'Bergen',
-        marker: {
-            symbol: 'diamond'
         },
-        data: [{
-            y: 1.5,
-            marker: {
-                symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
-            },
-            accessibility: {
-                description: 'Snowy symbol, this is the coldest point in the chart.'
+        yAxis: {
+            title: {
+                text: 'Price'
             }
-        }, 1.6, 3.3, 5.9, 10.5, 13.5, 14.5, 14.4, 11.5, 8.7, 4.7, 2.6]
-    }]
-});
+        },
+        tooltip: {
+            crosshairs: true,
+            shared: true
+        },
+        plotOptions: {
+            spline: {
+                marker: {
+                    radius: 4,
+                    lineColor: '#666666',
+                    lineWidth: 1
+                }
+            }
+        },
+        series: [{
+            name: 'Client Median Price',
+            data: clusterMedianPrices,
+            marker: {
+                symbol: 'square'
+            }
+        }, {
+            name: 'Client Market Price',
+            data: clientMarketPrices,
+            marker: {
+                symbol: 'diamond'
+            }
+        }]
+    }); 
 });
